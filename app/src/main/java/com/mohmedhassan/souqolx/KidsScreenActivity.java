b29fb2329633aa96356397f88cf02c577f12735d
MohamedHassan275 <mohamed209059@gmail.com>
2019-07-31 01:24:05 +0200

    Initial commit

--- app/src/main/java/com/mohmedhassan/souqolx/KidsScreenActivity.java
+++ app/src/main/java/com/mohmedhassan/souqolx/KidsScreenActivity.java
@@ -1,13 +1,69 @@
 package com.mohmedhassan.souqolx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.mohmedhassan.souqolx.Ads.AdDetaisActivity;
 
 public class KidsScreenActivity extends AppCompatActivity {
 
+    LinearLayout lineaelayout_clothes_kids_babies,lineaelayout_toys,lineaelayout_cribs_strollers,
+            lineaelayout_accessories, lineaelayout_others_kids_babies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kids_screen);
+
+        lineaelayout_clothes_kids_babies = findViewById(R.id.lineaelayout_clothes_kids_babies);
+        lineaelayout_toys = findViewById(R.id.lineaelayout_toys);
+        lineaelayout_cribs_strollers = findViewById(R.id.lineaelayout_cribs_strollers);
+        lineaelayout_accessories = findViewById(R.id.lineaelayout_accessories);
+        lineaelayout_others_kids_babies = findViewById(R.id.lineaelayout_others_kids_babies);
+
+        lineaelayout_clothes_kids_babies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_toys.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_cribs_strollers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_accessories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_others_kids_babies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+
+    }
+
+    private void IntentActivity() {
+
+        Intent intent = new Intent(KidsScreenActivity.this, AdDetaisActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/mohmedhassan/souqolx/MobileScreenActivity.java
+++ app/src/main/java/com/mohmedhassan/souqolx/MobileScreenActivity.java
@@ -1,13 +1,44 @@
 package com.mohmedhassan.souqolx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.mohmedhassan.souqolx.Ads.AdDetaisActivity;
 
 public class MobileScreenActivity extends AppCompatActivity {
 
+    LinearLayout lineaelayout_mobile_phone,lineaelayout_mobile_accessories;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_screen);
+
+        lineaelayout_mobile_phone = findViewById(R.id.lineaelayout_mobile_phone);
+        lineaelayout_mobile_accessories = findViewById(R.id.lineaelayout_mobile_accessories);
+
+        lineaelayout_mobile_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_mobile_accessories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+
+    }
+
+    private void IntentActivity() {
+
+        Intent intent = new Intent(MobileScreenActivity.this, AdDetaisActivity.class);
+        startActivity(intent);
     }
 }
