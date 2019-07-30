83c4964cc3c3f5dbe01b68638c0f56b6de1bd049
MohamedHassan275 <mohamed209059@gmail.com>
2019-07-31 01:10:03 +0200

    Initial commit

--- app/src/main/java/com/mohmedhassan/souqolx/PetsScreenActivity.java
+++ app/src/main/java/com/mohmedhassan/souqolx/PetsScreenActivity.java
@@ -1,13 +1,77 @@
 package com.mohmedhassan.souqolx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.mohmedhassan.souqolx.Ads.AdDetaisActivity;
 
 public class PetsScreenActivity extends AppCompatActivity {
 
+    LinearLayout lineaelayout_cats,lineaelayout_dogs,lineaelayout_horses,lineaelayout_fishes,lineaelayout_birds,lineaelayout_other_pets;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets_screen);
+
+        lineaelayout_cats = findViewById(R.id.lineaelayout_cats);
+        lineaelayout_dogs = findViewById(R.id.lineaelayout_dogs);
+        lineaelayout_horses = findViewById(R.id.lineaelayout_horses);
+        lineaelayout_fishes = findViewById(R.id.lineaelayout_fishes);
+        lineaelayout_birds = findViewById(R.id.lineaelayout_birds);
+        lineaelayout_other_pets = findViewById(R.id.lineaelayout_other_pets);
+
+        lineaelayout_cats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_dogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_horses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_fishes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_birds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_other_pets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+
+
+    }
+
+    private void IntentActivity() {
+
+        Intent intent = new Intent(PetsScreenActivity.this, AdDetaisActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/mohmedhassan/souqolx/SportingAndBikesScreenAcitivty.java
+++ app/src/main/java/com/mohmedhassan/souqolx/SportingAndBikesScreenAcitivty.java
@@ -1,13 +1,52 @@
 package com.mohmedhassan.souqolx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.mohmedhassan.souqolx.Ads.AdDetaisActivity;
 
 public class SportingAndBikesScreenAcitivty extends AppCompatActivity {
 
+    LinearLayout lineaelayout_sporting_goods,lineaelayout_bikes,lineaelayout_outdoor_equipment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spoting_goods_screen_acitivty);
+
+        lineaelayout_sporting_goods = findViewById(R.id.lineaelayout_sporting_goods);
+        lineaelayout_bikes = findViewById(R.id.lineaelayout_bikes);
+        lineaelayout_outdoor_equipment = findViewById(R.id.lineaelayout_outdoor_equipment);
+
+        lineaelayout_sporting_goods.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_bikes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+        lineaelayout_outdoor_equipment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentActivity();
+            }
+        });
+
+    }
+
+    private void IntentActivity() {
+
+        Intent intent = new Intent(SportingAndBikesScreenAcitivty.this, AdDetaisActivity.class);
+        startActivity(intent);
     }
 }
